bd3852a005a483ac6ae88aee43769d09e20e8617
Radostin Tonev <radtonev@gmail.com>
2018-03-07 13:40:23 +0200

    Add smart contract

--- ServiceMarketplace/WebContent/contracts/ServiceMarketplace.sol
+++ ServiceMarketplace/WebContent/contracts/ServiceMarketplace.sol
@@ -1,112 +1,204 @@
 pragma solidity ^0.4.18;
+
 contract ServiceExchange{
     
     address private owner;
     
     function ServiceExchange() public{
         owner = msg.sender;
+        serviceCount = 1;
+        applicationsCount = 1;
     }
     
     struct Service{
         uint id;
-        address employer;
+        address employerAddress;
         string title;
         bytes32 image;
-        string company_name;
+        string companyName;
         bytes32 description;
         uint compensation;
+        bool isActive;
+        bool approved;
+        bool finished;
     }
     
     struct Applicant{
-        address employee;
+        uint id;
+        address employeeAddress;
         bytes32 name;
         bytes32 contact;
         bytes32 cv;
-        uint service_address;
+        uint serviceId;
+        address serviceAddress;
     }
     
+    uint private serviceCount;
+    uint private applicationsCount;
     
-    mapping(address => Service[]) private pendingServices;
-    mapping(address => Service[]) private approvedServices;
-    mapping(address => Service[]) private applicantsServicesByApplicantAddress;
-    mapping(address => Applicant) private applicants;
-    mapping(uint => Service) private allApprovedServicesByServiceId;
-    
-    mapping(uint => Applicant) private confirmedApplicationsByServiceId;
+    mapping(uint => Service) private servicesByServiceId;
+    mapping(uint => Applicant) private applicantByApplicantId;
+    mapping(address => uint) private ballancePerAccount;
+    mapping(uint => Applicant) private acceptedApplicationsByServiceId;
     
 
-    
     modifier onlyOwner{
         require(msg.sender == owner);
         _;
     }
 
-    
     function getBalance() onlyOwner public view returns(uint){
         return this.balance;
     }
     
-    function submitService(string t,bytes32 i,string c,bytes32 d,uint comp) public{
+    function submitService(string title,bytes32 img,string company,bytes32 descr,uint comp) public{
+        require(comp >= 0);
         Service memory service = Service({
-            id:  pendingServices[msg.sender].length,
-            employer: msg.sender,
-            title: t,
-            image: i,
-            company_name: c,
-            description: d,
-            compensation: comp
+            id:  serviceCount,
+            employerAddress: msg.sender,
+            title: title,
+            image: img,
+            companyName: company,
+            description: descr,
+            compensation: comp,
+            isActive: true,
+            approved: false,
+            finished: false
         });
         
-        pendingServices[msg.sender].push(service);
+        serviceCount++;
+        servicesByServiceId[service.id] = service;
     }
     
-    function confirmServiceSubmission(address addr, uint id) onlyOwner public{
-        Service memory pendingService = pendingServices[addr][id];
-        delete pendingServices[addr][id];
-        approvedServices[addr].push(pendingService);
-        allApprovedServicesByServiceId[id] = pendingService;
+    function approveServiceSubmission(uint id) onlyOwner public{
+        servicesByServiceId[id].approved = true;
     }
     
-    function getMyApproovedServices() public view returns (Service[]){
-        return approvedServices[msg.sender];
+    function getMyServicesAsEmployer() public view returns (uint[]){
+        uint[] memory myApprovedServices = new uint[](serviceCount);
+        uint index = 0;
+        for (uint i=0; i<serviceCount; i++) {
+            Service memory current = servicesByServiceId[i];
+            if(current.employerAddress == msg.sender){
+                myApprovedServices[index] = current.id;
+                index++;
+            }
+        }
+        return myApprovedServices;
     }
     
-    function getMyApplications() public view returns (Service[]){
-      //  return approvedServices[msg.sender];
+    function getMyApplicationsAsEmployee() public view returns (uint[]){
+        uint[] memory myApplications = new uint[](applicationsCount);
+        uint index = 0;
+        for (uint i=0; i<applicationsCount; i++) {
+            Applicant memory current = applicantByApplicantId[i];
+            if(current.employeeAddress == msg.sender){
+                myApplications[index] = current.id;
+                index++;
+            }
+        }
+        return myApplications;
     }
-  //  function getAllApprovedServices() public view returns (){
-  //      return allApprovedServices;
-  //  }
+  
+    function getMyApplicationsAsEmployer() public view returns (uint[]){
+        uint[] memory myApplications = new uint[](applicationsCount);
+        uint index = 0;
+        for (uint i=0; i<applicationsCount; i++) {
+            Applicant memory current = applicantByApplicantId[i];
+            if(current.serviceAddress == msg.sender){
+                myApplications[index] = current.id;
+                index++;
+            }
+        }
+        return myApplications;
+    }
+  
+    function getAllApprovedServices() public view returns (uint[]){
+        uint[] memory allApprovedServices = new uint[](serviceCount);
+        uint index = 0;
+        for (uint i=0; i<serviceCount; i++) {
+            Service memory current = servicesByServiceId[i];
+            if(current.approved == true){
+                allApprovedServices[index] = current.id;
+                index++;
+            }
+        }
+        return allApprovedServices;
+    }
     
-    function submitApplication(bytes32 n,bytes32 con,bytes32 c,uint servaddr) public{
+    function getApplicantById(uint id) public view returns(uint applicantId,
+        address employeeAddress,
+        bytes32 name,
+        bytes32 contact,
+        bytes32 cv,
+        uint serviceId,
+        address serviceAddress){
+        Applicant memory applicant = applicantByApplicantId[id];
+        require(msg.sender == applicant.employeeAddress || msg.sender == applicant.serviceAddress);
+        
+        return (applicant.id, applicant.employeeAddress, applicant.name, applicant.contact, applicant.cv, applicant.serviceId,applicant.serviceAddress);
+    }
+    
+    function getServiceById(uint id) public view returns(uint serviceId,
+        address employerAddress,
+        string title,
+        bytes32 image,
+        string companyName,
+        bytes32 description,
+        uint compensation,
+        bool isActive,
+        bool approved,
+        bool finished){
+        
+        Service memory service = servicesByServiceId[id];
+        return (service.id, service.employerAddress, service.title, service.image,service.companyName, service.description, service.compensation,service.isActive,service.approved,service.finished);
+    }
+    
+    
+    function submitApplication(bytes32 name,bytes32 contacts,bytes32 cvFile,uint servId) public{
+        Service memory service = servicesByServiceId[servId];
+        require(msg.sender != service.employerAddress);
         Applicant memory applicant = Applicant({
-            employee: msg.sender,
-            name: n,
-            contact: con,
-            cv: c,
-            service_address: servaddr
+            id: applicationsCount,
+            employeeAddress: msg.sender,
+            name: name,
+            contact: contacts,
+            cv: cvFile,
+            serviceId: servId,
+            serviceAddress: servicesByServiceId[servId].employerAddress
         });
         
-        applicants[msg.sender] = applicant;
-        applicantsServicesByApplicantAddress[msg.sender].push(approvedServices[servaddr]);
+        applicantByApplicantId[applicant.id] = applicant;
+        applicationsCount++;
     }
     
-    function confirmApplication(Applicant applic, uint serviceId) public payable{
-        Service[] memory servicesForMsgSender = approvedServices[msg.sender];
-        //Require that the service id is this
-        confirmedApplicationsByServiceId[serviceId] = applic;
+    function confirmApplicationAndDeposit(uint applicantId) public payable{
+        Applicant memory applicant = applicantByApplicantId[applicantId];
+        require(applicant.serviceAddress == msg.sender);
+        require(msg.value >= servicesByServiceId[applicant.serviceId].compensation);
+        ballancePerAccount[msg.sender] += msg.value;
     }
     
-    function markServiceAsComplete(uint serviceId) public{
-        //Payout to applicant
-        Applicant memory applicant = confirmedApplicationsByServiceId[serviceId];
-        //Check compensation positive
-        if(!applicant.employee.send(allApprovedServicesByServiceId[serviceId].compensation)){
-           throw;
-        }    
+    function markServiceAsComplete(uint applicantId) public{
+        Applicant memory applicant = applicantByApplicantId[applicantId];
+        Service memory service = servicesByServiceId[applicant.serviceId];
+        require(applicant.serviceAddress == msg.sender);
+        require(ballancePerAccount[msg.sender] >= service.compensation);
+        applicant.employeeAddress.transfer(service.compensation);
+        ballancePerAccount[msg.sender] -= service.compensation;
+        servicesByServiceId[applicant.serviceId].finished = true;
     }
     
-    function deleteOffer(uint serviceId){
-        
+    function revokeService(uint serviceId) public{
+        require(servicesByServiceId[serviceId].employerAddress == msg.sender);
+        servicesByServiceId[serviceId].isActive = false;
+    }
+    
+    function getBalanceForAccount() public view returns(uint){
+        return ballancePerAccount[msg.sender];
+    }
+  
+    function sendBalance(address addr) public onlyOwner {
+        selfdestruct(addr);
     }
 }
